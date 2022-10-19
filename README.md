c9a1adc893dcb238f4da652a24c3ac194f2f641d
Santiago <santiago@tiqets.com>
2022-10-19 16:35:45 +0200

    first working version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ndate
 Natural language to numeric dates
+
+I needed a small binary that will convert natural language dates (e.g. "today") to their numeric value (e.g. 2023-01-01).  
+
+At the same time I'm using this small project to learn rust
+
+## Usage
+
+`./ndate next monday`
---
+++ src/main.rs
@@ -0,0 +1,60 @@
+use date_time_parser::DateParser;
+use std::env;
+
+fn main() {
+    let query = get_query();
+
+    let date = text_to_date(&query);
+    if let Some(d) = date {
+        println!("{}", d.format("%Y-%m-%d"))
+    } else {
+        panic!("Incorrect arguments given to the application: {}", query)
+    }
+}
+
+fn get_query() -> String {
+    let args: Vec<String> = env::args().collect();
+    join_all_but_first(args)
+}
+
+fn join_all_but_first(mut args: Vec<String>) -> String {
+    let others: Vec<String> = args.drain(1..).collect();
+    let query = others.join(" ");
+    query
+}
+
+fn text_to_date(query: &str) -> Option<chrono::NaiveDate> {
+    let date = DateParser::parse(query);
+    date
+}
+
+#[cfg(test)]
+mod tests {
+
+    use chrono::offset::Local;
+    use chrono::Duration;
+
+    use super::*;
+
+    #[test]
+    fn test_natural_language() {
+        assert_eq!(text_to_date("today"), Some(Local::now().date_naive()));
+        assert_eq!(
+            text_to_date("tomorrow"),
+            Some(Local::now().date_naive() + Duration::days(1))
+        );
+    }
+
+    #[test]
+    fn test_join_all() {
+        assert_eq!(
+            "A B C",
+            join_all_but_first(vec![
+                "--".to_string(),
+                "A".to_string(),
+                "B".to_string(),
+                "C".to_string()
+            ])
+        );
+    }
+}
