f7b514481c2c6de4ae338bc7e6c4333ff5ed0b79
Santiago <jsantiagoh@gmail.com>
2022-10-21 16:44:35 +0200

    docstrings and a constant

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
 use date_time_parser::DateParser;
 use std::env;
 
+const DEFAULT_DATE_FORMAT: &str = "%Y-%m-%d";
+
 fn main() {
-    let date_format = "%Y-%m-%d";
     let query = get_query();
 
     let date = text_to_date(&query).expect("date should have been parsable");
-    println!("{}", date.format(date_format))
+
+    println!("{}", date.format(DEFAULT_DATE_FORMAT))
 }
 
+/// Get the date query from command line arguments
 fn get_query() -> String {
     let args: Vec<String> = env::args().collect();
     join_all_but_first(args)
 }
 
+// Join all string elements of a vector into a single string but skip the first one
 fn join_all_but_first(mut args: Vec<String>) -> String {
     let others: Vec<String> = args.drain(1..).collect();
     let query = others.join(" ");
     query
 }
 
+// Converts a Natural Language date into a chrono::NaiveDate
 fn text_to_date(query: &str) -> Option<chrono::NaiveDate> {
     let date = DateParser::parse(query);
     date
