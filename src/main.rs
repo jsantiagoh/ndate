5a8d9313318a8ee35c073f8de0c8d9bb5d8f54f3
Santiago <santiago@tiqets.com>
2022-11-01 10:42:29 +0100

    Minor tweaks

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,43 @@
 use date_time_parser::DateParser;
 use std::env;
+use text_colorizer::*;
 
 const DEFAULT_DATE_FORMAT: &str = "%Y-%m-%d";
+const ERROR_ARGUMENTS: i32 = 1;
+const ERROR_PARSE: i32 = 2;
 
 fn main() {
-    let query = get_query();
+    let query = parse_args();
 
-    let date = text_to_date(&query).expect("date should have been parsable");
-
-    println!("{}", date.format(DEFAULT_DATE_FORMAT));
+    match text_to_date(&query) {
+        Ok(date) => println!("{}", date.format(DEFAULT_DATE_FORMAT)),
+        Err(_) => std::process::exit(ERROR_PARSE),
+    }
 }
 
-/// Get the date query from command line arguments
-fn get_query() -> String {
+fn print_usage() {
+    eprintln!(
+        "{} - Convert a Natural language date query into a concrete date.",
+        "ndate".green()
+    );
+    eprintln!("Usage: ndate <query>");
+}
+
+/// Parse Arguments and extract the query date
+fn parse_args() -> String {
     let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.len() == 0 {
+        print_usage();
+        std::process::exit(ERROR_ARGUMENTS);
+    }
+
     args.join(" ")
 }
 
 // Converts a Natural Language date into a chrono::NaiveDate
-fn text_to_date(query: &str) -> Option<chrono::NaiveDate> {
-    DateParser::parse(query)
+fn text_to_date(query: &str) -> Result<chrono::NaiveDate, String> {
+    DateParser::parse(query).ok_or(format!("Unable to parse query: {query}"))
 }
 
 #[cfg(test)]
@@ -32,10 +50,10 @@ mod tests {
 
     #[test]
     fn test_natural_language() {
-        assert_eq!(text_to_date("today"), Some(Local::now().date_naive()));
+        assert_eq!(text_to_date("today"), Ok(Local::now().date_naive()));
         assert_eq!(
             text_to_date("tomorrow"),
-            Some(Local::now().date_naive() + Duration::days(1))
+            Ok(Local::now().date_naive() + Duration::days(1))
         );
     }
 }
